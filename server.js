69c4ccc20472c182d294f05fe16b3a19a11c4596
Karthikeyan TS <keyashqa@gmail.com>
2025-06-01 00:35:22 +0530

    jj

--- server.js
+++ server.js
@@ -88,6 +88,6 @@ io.on('connection', (socket) => {
 
 // Start server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server is running on http://0.0.0.0:${PORT}`);
 });
